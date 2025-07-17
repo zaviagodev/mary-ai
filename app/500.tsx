d2b9af9b404b643f0ce36bffd2ce276c54fd8da6
umer2001 <umer2001.uf@gmail.com>
2025-07-17 22:41:21 +0500

    add: create 500 Server Error and Loading components

---
+++ app/500.tsx
@@ -0,0 +1,8 @@
+export default function Error500() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '10vh' }}>
+      <h1>500 - Server Error</h1>
+      <p>Sorry, something went wrong.</p>
+    </div>
+  );
+} 
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '10vh' }}>
+      <h1>Loading...</h1>
+    </div>
+  );
+} 
